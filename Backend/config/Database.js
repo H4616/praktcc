91fbbb2ff9b8a06efef92a3fd7497f40b9f3c3bf
H4616 <103203538+H4616@users.noreply.github.com>
2025-03-18 03:09:48 +0700

    docker

--- Backend/config/Database.js
+++ Backend/config/Database.js
@@ -1,8 +1,18 @@
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
 
 const db = new Sequelize('note_db', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
+    host: '35.188.204.2',
+    dialect: 'mysql',
+    port: 3306,
 });
+
+// Memeriksa koneksi ke database
+db.authenticate()
+    .then(() => {
+        console.log('Koneksi ke database berhasil!');
+    })
+    .catch((err) => {
+        console.error('Koneksi ke database gagal:', err);
+    });
 
 export default db;
